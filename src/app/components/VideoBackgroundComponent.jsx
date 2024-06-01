e523d5ae2e96e0fc1fb1277d9c83710947458a27
T0ny-dev <antonioraweb@gmail.com>
2024-06-01 17:28:17 -0600

    update video

--- src/app/components/VideoBackgroundComponent.jsx
+++ src/app/components/VideoBackgroundComponent.jsx
@@ -5,7 +5,7 @@ const VideoBackgroundComponent = () => {
   return (
     <div className="video-container">
       <video autoPlay muted loop className="background-video">
-        <source src="/bg.mp4" type="video/mp4" />
+        <source src="/bg3.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="video-overlay"></div>
